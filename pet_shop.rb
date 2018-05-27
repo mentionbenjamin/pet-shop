c1c585f41e16377d87b82a674c73a8ae798929c8
Ben Robinson <mentionbenjamin@gmail.com>
2018-05-27 09:30:03 +0100

    further homework submission

--- pet_shop.rb
+++ pet_shop.rb
@@ -22,8 +22,6 @@ def stock_count(pet_shop)
   return pet_shop[:pets].count()
 end
 
-
-
 def pets_by_breed(pet_shop, breed)
   pets = pet_shop[:pets]
   pets_filtered_by_breed = []
@@ -32,3 +30,44 @@ def pets_by_breed(pet_shop, breed)
   end
  return pets_filtered_by_breed
 end
+
+
+# def find_pet_by_name(pet_shop, specific_pet_name)
+#   pet_names = pet_shop[:pets]
+#   new_hash_to_hold_pet_name = []
+#   for actual_pet_name in pet_names
+#     new_hash_to_hold_pet_name << actual_pet_name if pet_names == specific_pet_name
+#   end
+#   return new_hash_to_hold_pet_name
+# end
+
+# def find_pet_by_name(pet_shop, specific_pet_name)
+#   where_to_search_for_pet_name = pet_shop[:pets][:name]
+#   if where_to_search_for_pet_name == specific_pet_name
+#     return true
+#   end
+# end
+
+# 10
+
+def find_pet_by_name(pet_shop, pet_name)
+  pets = pet_shop[:pets]
+  for pet in pets
+    return pet if pet[:name] == pet_name
+  end
+end
+
+# 11
+
+def find_pet_by_name(pet_shop, pet_name)
+  pets = pet_shop[:pets]
+  for pet in pets
+    return pet if pet[:name] == pet_name
+  end
+  return nil
+end
+
+# 12
+
+
+# 13
